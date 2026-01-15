ba123692a140eb6a14aaa0b52e7a979121a31b1a
zyxkad <zyxkad@gmail.com>
2026-01-14 21:40:24 -0700

    update serialize util

--- common/src/main/java/com/github/litermc/cosmicpluse/util/SerializeUtil.java
+++ common/src/main/java/com/github/litermc/cosmicpluse/util/SerializeUtil.java
@@ -1,70 +1,168 @@
 package com.github.litermc.cosmicpluse.util;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.nbt.DoubleTag;
+import net.minecraft.nbt.FloatTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.network.FriendlyByteBuf;
+
+import org.joml.Quaterniond;
+import org.joml.Quaterniondc;
+import org.joml.Quaternionf;
+import org.joml.Quaternionfc;
+import org.joml.Vector3d;
+import org.joml.Vector3dc;
 
 public final class SerializeUtil {
 	private SerializeUtil() {}
 
-	public static long[] vec3ToLongArray(Vec3 pos) {
-		return new long[]{
-			Double.doubleToRawLongBits(pos.x),
-			Double.doubleToRawLongBits(pos.y),
-			Double.doubleToRawLongBits(pos.z)
-		};
+	/** BlockPos **/
+
+	public static int[] blockPosToArray(final BlockPos v) {
+		return new int[]{v.getX(), v.getY(), v.getZ()};
 	}
 
-	public static Vec3 vec3FromLongArray(long[] arr) {
-		return new Vec3(
-			Double.longBitsToDouble(arr[0]),
-			Double.longBitsToDouble(arr[1]),
-			Double.longBitsToDouble(arr[2])
-		);
+	public static BlockPos arrayToBlockPos(final int[] array) {
+		return array.length == 3 ? new BlockPos(array[0], array[1], array[2]) : BlockPos.ZERO;
 	}
 
-	public static CompoundTag hitResultToNBT(HitResult hit) {
-		CompoundTag tag = new CompoundTag();
-		tag.putByte("Type", (byte) (hit.getType().ordinal()));
-		tag.putLongArray("Loc", vec3ToLongArray(hit.getLocation()));
-		if (hit instanceof BlockHitResult blockHit) {
-			tag.putLong("Pos", blockHit.getBlockPos().asLong());
-			tag.putByte("Dir", (byte) (blockHit.getDirection().get3DDataValue()));
-			if (hit.getType() == HitResult.Type.BLOCK) {
-				tag.putBoolean("Inside", blockHit.isInside());
-			}
-		} else if (hit instanceof EntityHitResult entityHit) {
-			tag.putInt("Entity", entityHit.getEntity().getId());
-		} else {
-			throw new IllegalArgumentException("Unsupported HitResult " + hit.getClass().toString());
-		}
+	/** Quaterniond **/
+
+	public static ListTag quaterniondToList(final Quaterniondc v) {
+		final ListTag tag = new ListTag();
+		tag.add(DoubleTag.valueOf(v.x()));
+		tag.add(DoubleTag.valueOf(v.y()));
+		tag.add(DoubleTag.valueOf(v.z()));
+		tag.add(DoubleTag.valueOf(v.w()));
 		return tag;
 	}
 
-	public static HitResult hitResultFromNBT(Level level, CompoundTag tag) {
-		HitResult.Type type = HitResult.Type.values()[tag.getByte("Type")];
-		return switch (type) {
-			case BLOCK -> new BlockHitResult(
-				vec3FromLongArray(tag.getLongArray("Loc")),
-				Direction.from3DDataValue(tag.getByte("Dir")),
-				BlockPos.of(tag.getLong("Pos")),
-				tag.getBoolean("Inside")
-			);
-			case MISS -> BlockHitResult.miss(
-				vec3FromLongArray(tag.getLongArray("Loc")),
-				Direction.from3DDataValue(tag.getByte("Dir")),
-				BlockPos.of(tag.getLong("Pos"))
-			);
-			case ENTITY -> new EntityHitResult(
-				level.getEntity(tag.getInt("Entity")),
-				vec3FromLongArray(tag.getLongArray("Loc"))
-			);
-		};
+	public static Quaterniond listToQuaterniond(final ListTag tag) {
+		return listToQuaterniond(tag, new Quaterniond());
+	}
+
+	public static Quaterniond listToQuaterniond(final ListTag tag, final Quaterniond dest) {
+		dest.x = tag.getDouble(0);
+		dest.y = tag.getDouble(1);
+		dest.z = tag.getDouble(2);
+		dest.w = tag.getDouble(3);
+		return dest;
+	}
+
+	public static void writeQuaterniond(final FriendlyByteBuf buf, final Quaterniondc v) {
+		buf.writeDouble(v.x());
+		buf.writeDouble(v.y());
+		buf.writeDouble(v.z());
+		buf.writeDouble(v.w());
+	}
+
+	public static Quaterniond readQuaterniond(final FriendlyByteBuf buf) {
+		return readQuaterniond(buf, new Quaterniond());
+	}
+
+	public static Quaterniond readQuaterniond(final FriendlyByteBuf buf, final Quaterniond dest) {
+		dest.x = buf.readDouble();
+		dest.y = buf.readDouble();
+		dest.z = buf.readDouble();
+		dest.w = buf.readDouble();
+		return dest;
+	}
+
+	/** Quaternionf **/
+
+	public static ListTag quaternionfToList(final Quaternionfc v) {
+		final ListTag tag = new ListTag();
+		tag.add(FloatTag.valueOf(v.x()));
+		tag.add(FloatTag.valueOf(v.y()));
+		tag.add(FloatTag.valueOf(v.z()));
+		tag.add(FloatTag.valueOf(v.w()));
+		return tag;
+	}
+
+	public static Quaternionf listToQuaternionf(final ListTag tag) {
+		return listToQuaternionf(tag, new Quaternionf());
+	}
+
+	public static Quaternionf listToQuaternionf(final ListTag tag, final Quaternionf dest) {
+		dest.x = tag.getFloat(0);
+		dest.y = tag.getFloat(1);
+		dest.z = tag.getFloat(2);
+		dest.w = tag.getFloat(3);
+		return dest;
+	}
+
+	public static void writeQuaternionf(final FriendlyByteBuf buf, final Quaternionfc v) {
+		buf.writeFloat(v.x());
+		buf.writeFloat(v.y());
+		buf.writeFloat(v.z());
+		buf.writeFloat(v.w());
+	}
+
+	public static Quaternionf readQuaternionf(final FriendlyByteBuf buf) {
+		return readQuaternionf(buf, new Quaternionf());
+	}
+
+	public static Quaternionf readQuaternionf(final FriendlyByteBuf buf, final Quaternionf dest) {
+		dest.x = buf.readFloat();
+		dest.y = buf.readFloat();
+		dest.z = buf.readFloat();
+		dest.w = buf.readFloat();
+		return dest;
+	}
+
+	/** Vector3d **/
+
+	public static ListTag vector3dToList(final Vector3dc v) {
+		final ListTag tag = new ListTag();
+		tag.add(DoubleTag.valueOf(v.x()));
+		tag.add(DoubleTag.valueOf(v.y()));
+		tag.add(DoubleTag.valueOf(v.z()));
+		return tag;
+	}
+
+	public static Vector3d listToVector3d(final ListTag tag) {
+		return listToVector3d(tag, new Vector3d());
+	}
+
+	public static Vector3d listToVector3d(final ListTag tag, final Vector3d dest) {
+		dest.x = tag.getDouble(0);
+		dest.y = tag.getDouble(1);
+		dest.z = tag.getDouble(2);
+		return dest;
+	}
+
+	public static CompoundTag putVector3d(final CompoundTag tag, final Vector3dc v) {
+		tag.putDouble("x", v.x());
+		tag.putDouble("y", v.y());
+		tag.putDouble("z", v.z());
+		return tag;
+	}
+
+	public static Vector3d getVector3d(final CompoundTag tag) {
+		return getVector3d(tag, new Vector3d());
+	}
+
+	public static Vector3d getVector3d(final CompoundTag tag, final Vector3d dest) {
+		dest.x = tag.getDouble("x");
+		dest.y = tag.getDouble("y");
+		dest.z = tag.getDouble("z");
+		return dest;
+	}
+
+	public static void writeVector3d(final FriendlyByteBuf buf, final Vector3dc v) {
+		buf.writeDouble(v.x());
+		buf.writeDouble(v.y());
+		buf.writeDouble(v.z());
+	}
+
+	public static Vector3d readVector3d(final FriendlyByteBuf buf) {
+		return readVector3d(buf, new Vector3d());
+	}
+
+	public static Vector3d readVector3d(final FriendlyByteBuf buf, final Vector3d dest) {
+		dest.x = buf.readDouble();
+		dest.y = buf.readDouble();
+		dest.z = buf.readDouble();
+		return dest;
 	}
 }
